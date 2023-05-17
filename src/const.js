91d63689e5a261f0eb33ed05e4da2283f138d002
Анна Андреева <andreeva.anna2909@gmail.com>
2023-05-17 23:55:16 +0300

    Добавлена структура данных для фильтров

--- src/const.js
+++ src/const.js
@@ -2,4 +2,11 @@ const TYPES_OF_POINT = ['Taxi', 'Bus', 'Train', 'Ship', 'Drive', 'Flight', 'Chec
 
 const POINT_COUNT = 5;
 
-export {TYPES_OF_POINT, POINT_COUNT};
+const FilterType = {
+  EVERYTHING: 'everything',
+  FUTURE: 'future',
+  PRESENT: 'present',
+  PAST: 'past',
+};
+
+export {TYPES_OF_POINT, POINT_COUNT, FilterType};
